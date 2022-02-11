474fd9edd7d114c28a4adf4055e6aef1dbe557dd
AdrianoValadaresFrwk <adrianovaladares@frwk.com.br>
2022-02-11 11:20:38 -0300

    Melhorias de layout e funcionalidade de contador no header

--- src/index.js
+++ src/index.js
@@ -1,27 +1,61 @@
-//get all the imgs in the "slide" div and change to an array of img objects
-const slide = document.getElementById("slide");
-var arr = Array.prototype.slice.call(slide.children);
+var map = L.map("map").setView([-29.746352, -52.3562802], 15);
+L.tileLayer("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 18,
+  minZoom: 3,
+}).addTo(map);
+var marker = L.marker([-29.746352, -52.3562802]).addTo(map);
 
-//initialize css
-arr.forEach(function (imgObj) {
-  imgObj.classList.add("slide-img");
+const countDown = setInterval(() => {
+  var now = new Date();
+  var eventDate = new Date(now.getFullYear(), 9, 24, 18, 0, 0);
+  var diff = eventDate - now;
+  var months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
+  // var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  // calculate the days left to the end of the month
+  var days = Math.floor(
+    (diff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+  );
+  var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  if (hours === 0 && minutes === 0 && seconds === 0) {
+    clearInterval(countDown);
+  }
+
+  document.getElementById("months").innerHTML = months;
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  document.getElementById("h-months").innerHTML = months;
+  document.getElementById("h-days").innerHTML = days;
+  document.getElementById("h-hours").innerHTML = hours;
+  document.getElementById("h-minutes").innerHTML = minutes;
+  document.getElementById("h-seconds").innerHTML = seconds;
+}, 1000);
+
+// var header = document.querySelector("header");
+// console.log(header);
+// document.body.removeChild(header);
+// header.classList.remove("d-none");
+
+document.addEventListener("scroll", () => {
+  try {
+    var scrollTop = document.documentElement.scrollTop;
+    // console.log(scrollTop);
+    var header = document.querySelector("header");
+    if (scrollTop < 100) {
+      header.classList.add("d-none");
+      // document.body.removeChild(header);
+    } else {
+      header.classList.remove("d-none");
+      // document.body.appendChild(header);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
-
-//showing the first slide
-arr[0].classList.add("show");
-
-//initializing values
-var currentSlide = 1;
-var slideLength = slide.children.length;
-var prevSlide = 0;
-
-//interval function
-setInterval(function () {
-  if (currentSlide >= slideLength) currentSlide = 0;
-
-  arr[prevSlide].classList.remove("show");
-  arr[currentSlide].classList.add("show");
-
-  prevSlide = currentSlide;
-  currentSlide++;
-}, 7000);
